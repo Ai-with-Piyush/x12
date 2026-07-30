0c1f3bb103993dff6a4b3296f5de4a7d20103c25
Piyush Shukla <piyush.1b.ai@gmail.com>
2026-07-31 03:40:44 +0530

    f

--- Frontend/src/Components/midd.jsx
+++ Frontend/src/Components/midd.jsx
@@ -6,11 +6,9 @@ function Midd() {
         <div className="text-black bg-white">
             <div className="flex flex-col items-center pt-10 md:pt-16 lg:pt-20 px-4 text-center">
                 <p className="font-bold rounded-md text-xs sm:text-sm flex px-4 sm:px-5 mt-6 md:mt-10">
-                    🎉New : Use Promo:<b className="bg-gray-400 font-bold rounded p-2 "> PIYUSH01 </b>  to get 40% Off!
+                    🎉New : Use<b className="bg-gray-400 font-bold rounded p-1 "> PIYUSH01</b> to get 40% Off!
                 </p>
-                <p className="font-bold rounded-md text-xs sm:text-sm flex px-4 sm:px-5 mt-2 md:mt-10">
-                    Use Promo:<b className="bg-gray-400 font-bold rounded p-1 "> PIYUSH01</b>  to get 40% Off!
-                </p>
+                
                 <h1 className="text-2xl md:text-4xl lg:text-6xl font-bold mt-4 md:mt-6">
                     Build consistent Design
                 </h1>
